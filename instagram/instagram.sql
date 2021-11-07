54fcd85f6056eca6f355ae4c893cdfeb282973b2
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-06 19:51:23 -0700

    Add materialized view for weekly_likes

--- instagram/instagram.sql
+++ instagram/instagram.sql
@@ -109,3 +109,20 @@ CREATE OR REPLACE VIEW recent_posts AS (
 	ORDER BY created_at DESC
 	LIMIT 15
 );
+
+
+CREATE MATERIALIZED VIEW weekly_likes AS(
+	SELECT
+		date_trunc('week', COALESCE(posts.created_at, comments.created_at)) AS week
+		, COUNT(posts.id) AS num_likes_for_posts
+		, COUNT(comments.id) AS num_likes_for_comments
+	FROM likes
+	LEFT JOIN posts ON posts.id = likes.post_id
+	LEFT JOIN comments ON comments.id = likes.comment_id
+	GROUP BY week
+	ORDER BY week
+) WITH DATA;
+
+
+-- to refresh materialized view
+-- REFRESH MATERIALIZED VIEW weekly_likes;
