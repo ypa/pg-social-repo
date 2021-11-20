e21080982130a6a68a10d449483174c525fe1f72
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-20 10:27:09 -0800

    Add query and close methods

--- api/social-repo/src/pool.js
+++ api/social-repo/src/pool.js
@@ -7,6 +7,15 @@ class Pool {
     this._pool = new pg.Pool(options);
     return this._pool.query('SELECT 1 + 1;');
   }
+
+  close() {
+    return this._pool.end();
+  }
+
+  // BIG SECURITY ISSUE HERE. TODO: make sql params
+  query(sql) {
+    return this._pool.query(sql);
+  }
 }
 
 module.exports = new Pool();
