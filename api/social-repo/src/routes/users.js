c9b24ba5b923c01e0a066a7b50a616dcd21577dc
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-20 11:39:52 -0800

    Add UserRepo and test out get users/ find method

--- api/social-repo/src/routes/users.js
+++ api/social-repo/src/routes/users.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const UserRepo = require('../repos/user-repo');
 
 const router = express.Router();
 
-router.get('/users', async (req, res) => {});
+router.get('/users', async (req, res) => {
+  const users = await UserRepo.find();
+
+  res.send(users);
+});
 
 router.get('/users/:id', async (req, res) => {});
 
