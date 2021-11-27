384e5130010901419596181f5cbca6eaddd7d80d
Yan P Aung <yan.pye.aung@gmail.com>
2021-11-27 12:10:07 -0800

    Refactor out DB schema creation logic into test Context class with static factory method and clean up method

--- api/social-repo/src/test/routes/users.test.js
+++ api/social-repo/src/test/routes/users.test.js
@@ -2,65 +2,15 @@ const request = require('supertest');
 const buildApp = require('../../app');
 const UserRepo = require('../../repos/user-repo');
 const pool = require('../../pool');
+const Context = require('../context');
 
-const { randomBytes } = require('crypto');
-const { default: migrate } = require('node-pg-migrate');
-const format = require('pg-format');
-
+let context;
 beforeAll(async () => {
-  // Randomly generating a role name to connect to PG as
-  const roleName = 'a' + randomBytes(4).toString('hex');
-
-  // Connect to PG as usual
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: 'ypa',
-    password: '',
-  });
-
-  // Create a new role
-  await pool.query(
-    format('CREATE ROLE %I WITH LOGIN PASSWORD %L;', roleName, roleName)
-  );
-
-  // Create a schema with the same name
-  await pool.query(
-    format('CREATE SCHEMA %I AUTHORIZATION %I;', roleName, roleName)
-  );
-
-  // Disconnect entirely from PG
-  await pool.close();
-
-  // Run our migrations in the new schema
-  await migrate({
-    schema: roleName,
-    direction: 'up',
-    log: () => {},
-    noLock: true,
-    dir: 'migrations',
-    databaseUrl: {
-      host: 'localhost',
-      port: 5432,
-      database: 'socialnetwork-test',
-      user: roleName,
-      password: roleName,
-    },
-  });
-
-  // Connect to PG as the newly created role
-  await pool.connect({
-    host: 'localhost',
-    port: 5432,
-    database: 'socialnetwork-test',
-    user: roleName,
-    password: roleName,
-  });
+  context = await Context.build();
 });
 
 afterAll(() => {
-  return pool.close();
+  return context.close();
 });
 
 it('create a user', async () => {
